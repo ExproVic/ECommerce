1a830d75593f20e1cacfee66d94b8fbca3289026
ExproVic <viktor19970429@gmail.com>
2024-01-15 10:36:56 +0100

    create message accept and check

--- app/src/main/java/com/example/ecommerce/emailfeedback/emailUserActivity.java
+++ app/src/main/java/com/example/ecommerce/emailfeedback/emailUserActivity.java
@@ -1,6 +1,9 @@
 package com.example.ecommerce.emailfeedback;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +11,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ecommerce.R;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,76 +25,138 @@ import java.util.List;
 
 public class emailUserActivity extends AppCompatActivity {
 
+    private String userId;
     private List<ReceiptItem> receiptItemList;
     private ReceiptAdapter receiptAdapter;
-    int country=0;
-
+    private int totalcount=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email_user);
 
-        // Отримайте посилання на RecyclerView
         RecyclerView recyclerView = findViewById(R.id.recycleviewEmail);
 
-        // Ініціалізуйте список ReceiptItem та адаптер
         receiptItemList = new ArrayList<>();
         receiptAdapter = new ReceiptAdapter(receiptItemList);
 
-        // Встановлюємо адаптер для RecyclerView
         recyclerView.setAdapter(receiptAdapter);
 
-        // Встановлюємо LayoutManager, якщо необхідно (наприклад, LinearLayoutManager)
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-
-
-
-        // Додавання слухача для отримання даних
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
-        String userId = "YiH8XrfcpdhEU6Q5ji8yQOfwH1o1"; // Замініть це на ваш userId
 
-        databaseReference.child("accept_delivery").child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser != null) {
+            userId = currentUser.getUid();
+            getReceiptData();
+        }
+        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("accept_delivery").child(userId);
+
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (!dataSnapshot.exists()) {
+                    showAlertMessage("No new messages");
+                    return;
+                }
+
+                if (dataSnapshot.child("decision").exists()) {
+                    String decision = dataSnapshot.child("decision").getValue(String.class);
+                    if ("Confirm".equals(decision)) {
+                        showConfirmationDialog(decision);
+                    } else if ("Cancel".equals(decision)) {
+                        showAlertMessage("Your purchase request was not confirmed, please try again.");
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+
+
+
+    }
+
+    private void showConfirmationDialog(String decision) {
+        String message;
+
+        if ("Confirm".equals(decision)) {
+            message = "Congratulations, your purchase has been confirmed and collected for delivery, so expect it soon.";
+        } else if ("Cancel".equals(decision)) {
+            message = "Your purchase request was not confirmed, please try again.";
+            showAlertMessage(message);
+            return;
+        } else {
+            // За замовчуванням
+            message = "Your purchase status is unknown.";
+        }
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Confirmation");
+        builder.setMessage(message);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss(); // Закриваєте діалогове вікно
+            }
+        });
+
+        builder.show();
+    }
+
+
+    private void showAlertMessage(String message) {
+        Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_INDEFINITE);
+        snackbar.setAction("OK", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                snackbar.dismiss();
+            }
+        });
+        snackbar.show();
+    }
+
+
+
+    private void getReceiptData() {
+        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("accept_delivery").child(userId);
+
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()) {
                     receiptItemList.clear();
-                    double totalAmount = 0.0; // Ініціалізація totalAmount
+                    double totalAmount = 0.0;
 
-// Перевірка і додавання інших продуктів в список
                     for (DataSnapshot productSnapshot : dataSnapshot.getChildren()) {
-                        // Перевірка, чи існують необхідні підвузли в базі даних
                         if (productSnapshot.child("Pname").exists() && productSnapshot.child("count").exists() &&
                                 productSnapshot.child("price").exists() && !productSnapshot.hasChild("decision") &&
                                 !productSnapshot.hasChild("totalAmount")) {
 
-                            // Отримання значень полів з бази даних
                             String receiptName = productSnapshot.child("Pname").getValue(String.class);
                             Integer countInteger = productSnapshot.child("count").getValue(Integer.class);
-                            country=country+countInteger;
+                            totalcount=countInteger+totalcount;
+                            int receiptCount = (countInteger != null) ? countInteger : 0;
+
                             Double priceDouble = productSnapshot.child("price").getValue(Double.class);
+                            double receiptPrice = (priceDouble != null) ? priceDouble : 0.0;
 
-                            // Перевірка, чи значення не є null, перед використанням
-                            int receiptCount = (countInteger != null) ? countInteger.intValue() : 0;
-                            double receiptPrice = (priceDouble != null) ? priceDouble.doubleValue() : 0.0;
-
-                            // Додавання кількості товарів до загальної суми
                             totalAmount += receiptCount * receiptPrice;
 
-                            // Створення нового об'єкту ReceiptItem
                             ReceiptItem receiptItem = new ReceiptItem(receiptName, receiptCount, receiptPrice);
-
-                            // Додавання об'єкту ReceiptItem в кінець списку
                             receiptItemList.add(receiptItem);
                         }
                     }
-                    ReceiptItem totalAmountItem = new ReceiptItem("Total Amount", country, totalAmount);
 
+                    ReceiptItem totalAmountItem = new ReceiptItem("Total Amount", totalcount, totalAmount);
                     receiptItemList.add(totalAmountItem);
 
-
-                    // Сповіщення адаптера про зміни в списку
                     receiptAdapter.notifyDataSetChanged();
                 } else {
                     // Обробка випадку, коли немає даних для конкретного користувача
@@ -101,6 +169,6 @@ public class emailUserActivity extends AppCompatActivity {
                 // Можете додати код обробки помилок тут, якщо потрібно
             }
         });
-
     }
+
 }
